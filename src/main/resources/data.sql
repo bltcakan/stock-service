daae9e9e190edf1f80c52e787f3b3f25a352b596
bulutcakan <bulutck41@gmail.com>
2024-09-06 21:15:06 +0300

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO customer (username, password, role) VALUES ('admin', '$2a$10$DowJQ1XJ5pzFGmY6GfZrAue5AOWzfpwGBRqlqzqxXFWl5w1ZqMEvW', 'ADMIN');
-INSERT INTO customer (username, password, role) VALUES ('user1', '$2a$10$DowJQ1XJ5pzFGmY6GfZrAue5AOWzfpwGBRqlqzqxXFWl5w1ZqMEvW', 'USER');
-INSERT INTO customer (username, password, role) VALUES ('user2', '$2a$10$DowJQ1XJ5pzFGmY6GfZrAue5AOWzfpwGBRqlqzqxXFWl5w1ZqMEvW', 'USER');
+INSERT INTO customer (username, password, role) VALUES ('admin', '{noop}admin123', 'ADMIN');
+INSERT INTO customer (username, password, role) VALUES ('user1', '{noop}admin123', 'USER');
+INSERT INTO customer (username, password, role) VALUES ('user2', '{noop}admin123', 'USER');
 
 INSERT INTO asset (customer_id, asset_type, size, usable_size) VALUES ((SELECT id FROM customer WHERE username='user1'), 'TRY', 10000, 10000);
 INSERT INTO asset (customer_id, asset_type, size, usable_size) VALUES ((SELECT id FROM customer WHERE username='user2'), 'TRY', 10000, 10000);
